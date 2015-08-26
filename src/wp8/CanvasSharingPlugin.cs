2e6d52a64bafa7cca04c66bc6f44924ad4138032
Cyril Moralès <palmsnipe@hotmail.com>
2015-08-26 13:14:38 +0300

    test

--- src/wp8/CanvasSharingPlugin.cs
+++ src/wp8/CanvasSharingPlugin.cs
@@ -10,88 +10,93 @@ using Microsoft.Phone.Tasks;
 //  using System.Xml;
 //  using System.Xml.Linq;
 
-public class CanvasSharingPlugin : BaseCommand
+namespace Cordova.Extension.Commands
 {
-    public CanvasSharingPlugin()
-	{
-	}
 
-    public void saveImageDataToLibrary(string jsonArgs)
+    public class CanvasSharingPlugin : BaseCommand
     {
-        try
+        public CanvasSharingPlugin()
         {
-            var options = JsonHelper.Deserialize<string[]>(jsonArgs);
-
-            string imageData = options[0];
-            byte[] imageBytes = Convert.FromBase64String(imageData);
-
-            using (var imageStream = new MemoryStream(imageBytes))
+        }
+    
+        public void saveImageDataToLibrary(string jsonArgs)
+        {
+            try
             {
-                imageStream.Seek(0, SeekOrigin.Begin);
-
-                string fileName = String.Format("kortti_{0:yyyyMMdd_HHmmss}", DateTime.Now);
-                var library = new MediaLibrary();
-                var picture = library.SavePicture(fileName, imageStream);
-
-                if (picture.Name.Contains(fileName))
+                var options = JsonHelper.Deserialize<string[]>(jsonArgs);
+    
+                string imageData = options[0];
+                byte[] imageBytes = Convert.FromBase64String(imageData);
+    
+                using (var imageStream = new MemoryStream(imageBytes))
                 {
-                    DispatchCommandResult(new PluginResult(PluginResult.Status.OK, picture.GetPath()));
-                }
-                else
-                {
-                    DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR, 
-                        "Failed to save image: " + picture.Name));
+                    imageStream.Seek(0, SeekOrigin.Begin);
+    
+                    string fileName = String.Format("kortti_{0:yyyyMMdd_HHmmss}", DateTime.Now);
+                    var library = new MediaLibrary();
+                    var picture = library.SavePicture(fileName, imageStream);
+    
+                    if (picture.Name.Contains(fileName))
+                    {
+                        DispatchCommandResult(new PluginResult(PluginResult.Status.OK, picture.GetPath()));
+                    }
+                    else
+                    {
+                        DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR, 
+                            "Failed to save image: " + picture.Name));
+                    }
                 }
             }
+            catch (Exception ex)
+            {
+                DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR, ex.Message));
+            }
         }
-        catch (Exception ex)
-        {
-            DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR, ex.Message));
-        }
-    }
-
-    public void sharePicture(string jsonArgs)
-    {
-        try
-        {
-            var options = JsonHelper.Deserialize<string[]>(jsonArgs);
-
-            string path = options[0];
-            Microsoft.Phone.Tasks.ShareMediaTask smt = new ShareMediaTask();
-            smt.FilePath = path; 
-            smt.Show();
-            
-            DispatchCommandResult(new PluginResult(PluginResult.Status.OK, path));
-        }
-        catch (Exception ex)
-        {
-            DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR, ex.Message));
-        }
-    }
-
-    public void appVersion(string jsonArgs)
-    {
-        DispatchCommandResult(new PluginResult(PluginResult.Status.OK, "toto"));
-        //  try
-        //  {
-        //      //  var options = JsonHelper.Deserialize<string[]>(jsonArgs);
-        //      DispatchCommandResult(new PluginResult(PluginResult.Status.OK, "toto"));
-        //      //string version = XDocument.Load("WMAppManifest.xml").Root.Element("App").Attribute("Version").Value;
-
-        //      //if (String.IsNullOrEmpty(version))
-        //      //{
-        //      //    DispatchCommandResult(new PluginResult(PluginResult.Status.OK, version));
-        //      //}
-        //      //else
-        //      //{
-        //      //    DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR,
-        //      //        "Unable to get the version number"));
-        //      //}
-        //  }
-        //  catch (Exception ex)
-        //  {
-        //      DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR, ex.Message));
-        //  }
-    }
     
+        public void sharePicture(string jsonArgs)
+        {
+            try
+            {
+                var options = JsonHelper.Deserialize<string[]>(jsonArgs);
+    
+                string path = options[0];
+                Microsoft.Phone.Tasks.ShareMediaTask smt = new ShareMediaTask();
+                smt.FilePath = path; 
+                smt.Show();
+                
+                DispatchCommandResult(new PluginResult(PluginResult.Status.OK, path));
+            }
+            catch (Exception ex)
+            {
+                DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR, ex.Message));
+            }
+        }
+    
+        public void appVersion(string jsonArgs)
+        {
+            DispatchCommandResult(new PluginResult(PluginResult.Status.OK, "toto"));
+            //  try
+            //  {
+            //      //  var options = JsonHelper.Deserialize<string[]>(jsonArgs);
+            //      DispatchCommandResult(new PluginResult(PluginResult.Status.OK, "toto"));
+            //      //string version = XDocument.Load("WMAppManifest.xml").Root.Element("App").Attribute("Version").Value;
+    
+            //      //if (String.IsNullOrEmpty(version))
+            //      //{
+            //      //    DispatchCommandResult(new PluginResult(PluginResult.Status.OK, version));
+            //      //}
+            //      //else
+            //      //{
+            //      //    DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR,
+            //      //        "Unable to get the version number"));
+            //      //}
+            //  }
+            //  catch (Exception ex)
+            //  {
+            //      DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR, ex.Message));
+            //  }
+        }
+        
+    }
+
 }
