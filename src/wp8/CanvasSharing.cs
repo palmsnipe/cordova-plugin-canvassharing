91dafbef14bfb7751c87c2fb5fcda04441b6774d
Cyril Moralès <palmsnipe@hotmail.com>
2015-08-26 14:15:53 +0300

    test

--- src/wp8/CanvasSharing.cs
+++ src/wp8/CanvasSharing.cs
@@ -77,26 +77,58 @@ namespace Cordova.Extension.Commands
         {
             try
             {
-                //  var options = JsonHelper.Deserialize<string[]>(jsonArgs);
-                //  DispatchCommandResult(new PluginResult(PluginResult.Status.OK, "toto"));
-                //  AssemblyName asmName = new AssemblyName(Assembly.GetExecutingAssembly().FullName);
-                //  string version = asmName.Version.ToString();
-                //  var version = Package.Current.Id.Version;
-                //  String version = System.Reflection.Assembly.GetExecutingAssembly()
-                //      .FullName.Split('=')[1].Split(',')[0];
-                var nameHelper = new AssemblyName(Assembly.GetExecutingAssembly().FullName);
-                var version = nameHelper.Version;
-                //  string version = XDocument.Load("WMAppManifest.xml").Root.Element("App").Attribute("Version").Value;
+                string version = XDocument.Load("WMAppManifest.xml").Root.Element("App").Attribute("Version").Value;
     
-                if (String.IsNullOrEmpty(version))
+                DispatchCommandResult(new PluginResult(PluginResult.Status.OK, version));
+            }
+            catch (Exception ex)
+            {
+                DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR, ex.Message));
+            }
+        }
+    
+        public void sendEmail(string jsonArgs)
+        {
+            try
+            {
+                var options = JsonHelper.Deserialize<string[]>(jsonArgs);
+    
+                string title = options[0];
+                string description = options[1];
+                string path = options[2];
+                //  Microsoft.Phone.Tasks.ShareMediaTask smt = new ShareMediaTask();
+                //  smt.FilePath = path; 
+                //  smt.Show();
+                
+                // Send an Email with attachment
+                EmailMessage email = new EmailMessage();
+                //  email.To.Add(new EmailRecipient("test@developerpublish.com"));
+                email.Subject = title;
+                email.Body = description;
+                
+                StorageFile fl = await StorageFile.GetFileFromPathAsync(path);
+                String attachmentFile = fl.Name;
+
+                if (fl != null && attachmentFile != "")
                 {
-                    DispatchCommandResult(new PluginResult(PluginResult.Status.OK, version));
+                    try
+                    {
+                        var rastream = RandomAccessStreamReference.CreateFromFile(fl);
+                        var attachment = new EmailAttachment(attachmentFile, rastream);
+                        msg.Attachments.Add(attachment);
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.WriteLine("[C#] attachment exception: " + e.Message);
+                    }
                 }
-                else
-                {
-                    DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR,
-                        "Unable to get the version number"));
-                }
+                
+                //  var file = await GetTextFile();
+                //  email.Attachments.Add(new EmailAttachment(file.Name, file));
+                await EmailManager.ShowComposeNewEmailAsync(email);
+
+                
+                DispatchCommandResult(new PluginResult(PluginResult.Status.OK, path));
             }
             catch (Exception ex)
             {
--- www/CanvasSharing.js
+++ www/CanvasSharing.js
@@ -35,6 +35,18 @@ module.exports = {
         else {
             return cordova.exec(successCallback, failureCallback, "CanvasSharing","appVersion",[]);
         }
+    },
+    sendEmail: function(title, description, picturepath, successCallback, failureCallback) {
+        // successCallback required
+        if (typeof successCallback != "function") {
+            console.log("CanvasSharingPlugin Error: successCallback is not a function");
+        }
+        else if (typeof failureCallback != "function") {
+            console.log("CanvasSharingPlugin Error: failureCallback is not a function");
+        }
+        else {
+            return cordova.exec(successCallback, failureCallback, "CanvasSharing","appVersion",[title, description, picturepath]);
+        }
     }
 };
   
